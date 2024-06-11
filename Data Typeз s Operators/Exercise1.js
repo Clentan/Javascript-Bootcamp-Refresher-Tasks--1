c898bf0dd6d6a5f58e021adbda63d8ead1a89585
Clentan <105500834+Clentan@users.noreply.github.com>
2024-06-11 20:10:35 +0200

    adding

---
+++ Data Typeз s Operators/Exercise1.js
@@ -0,0 +1,17 @@
+/*Exercise 1: Numbers
+1. Declare a number variable and assign it a value.
+2. Declare a number variable and assign it a floating number.
+3. Perform addition, subtraction, multiplication, Division, 
+modulus and exponentiation with number variable and 
+another number.
+4. Print all your variables.*/
+
+const number1 = 12;
+const number2 = 25.99999;
+const Sum = number1 + number2;
+const Subtraction = number1 - number2;
+const Multiplication = number1 * number2;
+const Division = number1 / number2;
+const Modulus = number1 % number2;
+const Exponentiation = number1 ** number2;
+console.log(`Sum= ${Sum}, Subtraction= ${Subtraction} , Multiplication= ${Multiplication},Division= ${Division},Modulus= ${Modulus} , Exponentiation= ${Exponentiation}`);
