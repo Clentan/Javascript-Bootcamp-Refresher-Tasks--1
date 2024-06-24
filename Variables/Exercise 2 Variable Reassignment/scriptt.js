fc922e4a9c6cf9debc322125742a8a8e0c41e3a0
CLENTAN CHAUKE <105500834+Clentan@users.noreply.github.com>
2024-06-25 00:18:01 +0200

    Delete Variables directory

--- Variables/Exercise 2 Variable Reassignment/scriptt.js
+++
@@ -1,5 +0,0 @@
-let favoriteColour;
-favoriteColour = 'Black';
-console.log(favoriteColour);
-favoriteColour = 'white';
-console.log(favoriteColour);
--- Variables/Exercise 3 Arithmetic Operations/exercise3.js
+++
@@ -1,7 +0,0 @@
-const num1 = 12;
-const num2 = 13;
-const sum = num1 + num2;
-const difference = num1 - num2;
-const product = num1 * num2;
-const quotient = num1 / num2;
-console.log('Sum=' + sum, ':Difference=' + difference, ':Product=' + product, ':Quotient=' + quotient);
